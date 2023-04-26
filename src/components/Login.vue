1142fd4fd7946a1c6c983132ec0744c166ffca29
Rodevp <rodriguezrodrigor12@gmail.com>
2023-04-25 21:42:47 -0500

    login styles complete

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,6 +1,7 @@
 <script lang="ts" setup>
 import { ref } from 'vue'
 import BaseInput from './common/BaseInput.vue';
+import BaseButton from './common/BaseButton.vue';
 
 const email = ref<string>('')
 const password = ref<string>('')
@@ -29,22 +30,27 @@ const handlePasword = (value: string) => {
         </h3>
 
         <form>
-            <BaseInput
-                type="email"
-                placeholder="Email"
-                label="Email"
-                :update="handleEmail"
-                :text="email"
-            />
-            <BaseInput
-                type="password"
-                placeholder="Contraseña"
-                label="Contraseña"
-                :update="handlePasword"
-                :text="password"
-            />
-
+            <BaseInput type="email" placeholder="Email" label="Email" :update="handleEmail" :text="email" />
+            <BaseInput type="password" placeholder="Contraseña" label="Contraseña" :update="handlePasword"
+                :text="password" />
+            <div class="restarMsg">
+                <p class="restartPassword">¿Olvido la contraseña?</p>
+            </div>
+            <BaseButton text="Acceder" />
         </form>
+        <div class="socials">
+            <div></div>
+            <h3>Logueate con RRSS</h3>
+            <div></div>
+        </div>
+        <div class="socials_logo">
+            <img src="../assets/instragram.png" alt="instragram logo" width="51" height="51" />
+            <img src="../assets/twitter.png" alt="twitter logo" width="51" height="51" />
+            <img src="../assets/fb.png" alt="facebook logo" width="51" height="51" />
+        </div>
+        <p class="register">
+            Quieres registrarte? <span>Registrate</span>
+        </p>
     </section>
 </template>
 
@@ -77,6 +83,70 @@ const handlePasword = (value: string) => {
     text-align: center;
     letter-spacing: 0.065em;
     color: #FFFFFF;
+}
+
+.restartPassword {
+    font-style: normal;
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 21px;
+    text-align: center;
+    color: #E487FB;
+}
+
+.restarMsg {
+    display: flex;
+    justify-content: end;
+    margin-bottom: 0.5rem;
+}
+
+.socials {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 0 1rem;
+    margin: 1rem 0;
+}
+
+.socials>div {
+    width: 100px;
+    height: 2px;
+    margin: 0 0.5rem;
+    background-color: #FFFFFF;
+}
+
+.socials>h3 {
+    width: 110px;
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 21px;
+    text-align: center;
+    color: #FFFFFF;
+}
+
+.socials_logo {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 1rem;
+}
+
+.socials_logo img {
+    margin: 0 1rem;
+    cursor: pointer;
+}
+
+.register {
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 19px;
+    text-align: center;
+    letter-spacing: 0.065em;
+    color: #FFFFFF;
+}
+
+.register > span {
+    color: #E487FB;;
 }
 </style>
 
---
+++ src/components/common/BaseButton.vue
@@ -0,0 +1,24 @@
+<script lang="ts" setup>
+
+interface Props {
+    text: string
+    handleClick?: Function
+}
+
+const props = defineProps<Props>()
+
+</script>
+
+<template>
+    <button @click="props.handleClick">{{ props.text }}</button>
+</template>
+
+<style scoped>
+button {
+    width: 300px;
+    height: 60px;
+    border-radius: 10px;
+    background: #E487FB;
+    color: #3E1149;
+}
+</style>
